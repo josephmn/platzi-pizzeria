6afd599c0c82c94b4fcac7a858357b229c2e0912
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-22 17:05:53 -0500

    usando @Modifying en un @Query, para actualizar precio - Pizza

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -1,7 +1,11 @@
 package com.platzi.pizza.persistence.repository;
 
 import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.service.dto.UpdatePizzaPriceDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,4 +45,26 @@ public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer
      * Query Method -> Pizza mas baratas de la pizzeria, solo top 3
      */
     List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
+
+    /*
+     * Update price Pizza with SQL Native, this work
+     */
+    /*@Query(value =
+            "UPDATE pizza " +
+                    "SET price = :newPrice " +
+                    "WHERE id_pizza = :idPizza", nativeQuery = true)
+     void updatePrice(@Param("idPizza") int idPizza, @Param("newPrice") double newPrice);
+     *
+     */
+
+    /*
+     * Update price Pizza with SQL Native, Using Spring Special Language
+     */
+    @Query(value =
+            "UPDATE pizza " +
+                    "SET price = :#{#newPizzaPrice.newPrice} " +
+                    "WHERE id_pizza = :#{#newPizzaPrice.pizzaId}", nativeQuery = true)
+    @Modifying
+    void updatePrice(@Param("newPizzaPrice") UpdatePizzaPriceDto newPizzaPrice);
+
 }
