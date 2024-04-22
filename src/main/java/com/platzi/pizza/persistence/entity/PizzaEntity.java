3f5089849f2d3764ba8fbcc66d60ae2c7a0852f7
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-22 18:11:16 -0500

    Auditoria con Spring Data

--- src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -2,15 +2,23 @@ package com.platzi.pizza.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class PizzaEntity {
-
+public class PizzaEntity extends AuditableEntity {
+    /*
+     * Para poder usar Auditorias,
+     * primero agregar la anotacion: @EnableJpaAuditing en el main principal
+     * luego crear la clase AuditableEntity y agregar los campos necesario
+     * despues ingresar la anotacion @EntityListeners(AuditingEntityListener.class)
+     * apuntando a AuditingEntityListener.class
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
