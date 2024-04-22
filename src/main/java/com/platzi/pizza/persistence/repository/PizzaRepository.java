9a34b312303904e3b2398ba454dd181028363aa2
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-21 23:07:21 -0500

    usando Query Method - Pizza - And, Or, True, IngnoreCase, OrderBy

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -3,5 +3,16 @@ package com.platzi.pizza.persistence.repository;
 import com.platzi.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+    /*
+     * Query Method -> Pizza Available True and Order By Price
+     */
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+
+    /*
+     * Query Method -> Pizza by Name
+     */
+    List<PizzaEntity> findAllByAvailableTrueAndNameIgnoreCase(String name);
 }
